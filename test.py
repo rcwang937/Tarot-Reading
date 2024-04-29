cb0adb48d2d9aef1d91aafc3824d5fab445b2db3
SiqiiW <sw8495#nyu.edu>
2024-04-29 02:50:01 -0400

    happi happi happi

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-username = os.getenv("MONGODB_URI")
-password = os.getenv("MONGO_DB_PASSWORD")
-connection_string = os.getenv("MONGO_DB_CONNECTION_STRING")
-
-print(username, password, connection_string)
-print("f")
