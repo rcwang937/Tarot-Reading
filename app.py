321deb62f9db01501ce077b458fefe553cfb8d12
rcwang937 <alicewang2801@gmail.com>
2024-04-28 22:44:12 -0400

    added mongodb and finetuned model

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from tarot_deck import TarotDeck
 from openai import OpenAI
 from dotenv import load_dotenv
-
+from pymongo import MongoClient
 import base64
 import time
 from PIL import Image
@@ -14,7 +14,27 @@ load_dotenv()
 with open( "style.css" ) as css:
     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# Connect to MongoDB
+def get_db():
+    client = MongoClient(os.getenv("MONGODB_URI"))
+    db = client.tarot_app
+    return db
+
+# Function to save user data to MongoDB
+def save_user_data(question, cards, reading_type, reading_content):
+    db = get_db()
+    user_data = {
+        "question": question,
+        "cards": cards,
+        "reading_type": reading_type,
+        "reading_content": reading_content,
+        "timestamp": time.time()
+    }
+    db.user_readings.insert_one(user_data)
+
+#OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY_1")
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 
@@ -30,15 +50,12 @@ def main():
     if mode == 'Classic':
         user_question = st.text_input("Enter your question:")
         if user_question:
+
+            # shuffle card
             tarot_deck = TarotDeck()
             tarot_deck.shuffle_cards()
 
-            # st.button("Shuffle Cards")
-
-            # # Simulate card shuffling animation
-            # if st.button("Stop Shuffling"):
-            #     pass
-
+            # display card shuffling options
             col1, col2 = st.columns(2)
             with col1:
                 if st.button("Shuffle Cards"):
@@ -52,24 +69,7 @@ def main():
             if st.session_state.get('shuffling', False):
                 st.write("Card shuffling...")
 
-            # selected_indices = st.multiselect("Choose your cards:", options=range(78), format_func=lambda x: "Card " + str(x+1))
-
-
-            # if len(selected_indices) == 3:
-            #     drawn_cards = [tarot_deck.shuffled_deck[i] for i in selected_indices]
-            #     images = []
-            #     captions = []
-            #     for card in drawn_cards:
-            #         image_path = os.path.join( card[2])
-            #         img = Image.open(image_path)
-            #         if card[1] == 'downward':
-            #             img = img.rotate(180)
-            #         images.append(img)
-            #         captions.append(card[0])
-
-            #     st.image(images, width=200, caption=captions)
-
-            selected_indices = st.multiselect("Choose your cards:", options=range(78), format_func=lambda x: "Card " + str(x+1))
+            selected_indices = st.multiselect("The order of your selection matters.\n\nChoose your 3 cards:", options=range(78), format_func=lambda x: "Card " + str(x+1))
 
             if len(selected_indices) == 3:
                 drawn_cards = [tarot_deck.shuffled_deck[i] for i in selected_indices]
@@ -91,8 +91,26 @@ def main():
 
 
                 # Call reading function
-                reading = get_tarot_reading_v1(user_question, drawn_cards)
-                ReadingWrite("Your Tarot Reading:\n"+reading)
+                reading_ft= get_tarot_reading_finetune(user_question, drawn_cards)
+                reading_normal= get_tarot_reading_normal(user_question, drawn_cards)
+
+                ReadingWrite("Your Tarot Reading Finetune Model:\n\n"+reading_ft)
+                ReadingWrite("Your Tarot Reading Normal Model:\n\n"+reading_normal)
+
+                # Allow user to choose which reading they prefer
+                reading_choice = st.radio("Choose the reading you prefer:", ('Finetuned Model', 'Normal Model'))
+                if st.button("Confirm Choice"):
+                    # Determine the content of the chosen reading
+                    if reading_choice == 'Finetuned Model':
+                        chosen_reading_content = reading_ft
+                    else:
+                        chosen_reading_content = reading_normal
+
+                    # Save user data including their choice and the reading content
+                    card_names = [card[0] for card in drawn_cards]  # Extract card names for storage
+                    save_user_data(user_question, card_names, reading_choice, chosen_reading_content)
+                    st.success("Your choice and session details have been saved.")
+
 
     elif mode == 'Fun':
         if 'stage' not in st.session_state:
@@ -127,26 +145,31 @@ def main():
                 reading = FunMode.get_tarot_reading_fun(keywords, user_question)
                 ReadingWrite("Your Tarot Reading:\n"+reading)
 
-# def get_tarot_reading_v1(user_question,drawn_cards):
-
-#     prompt = tarot_deck.generate_prompt(user_question, drawn_cards)
-
-#     response = client.chat.completions.create(
-#         model="gpt-3.5-turbo-0125",
-#         #response_format={ "type": "json_object" },
-#         messages=[
-#             {"role": "system", "content": "You are a tarot reader. Please interpret the cards drawn for the user. 1. Provide the card info and the upward or downward status of each card. 2. Give an explaination of each individual card, and then a overall analysis based on these three cards."},
-#             {"role": "user", "content": prompt}
-#         ]
-#     )
-#     return response.choices[0].message.content
-
-def get_tarot_reading_v1(user_question,drawn_cards):
+def get_tarot_reading_normal(user_question,drawn_cards):
 
     prompt = tarot_deck.generate_prompt(user_question, drawn_cards)
 
     response = client.chat.completions.create(
         model="gpt-4",
+        #response_format={ "type": "json_object" },
+        messages=[
+            {"role": "system", "content": " You are a tarot reader. Please interpret the cards drawn for the user."
+                                        + " Provide the card info and the upward or downward status of each card."
+                                        + " Give an explaination of each individual card, and then a overall analysis based on these three cards."
+                                        + " Please feel free to use the elments in the cards as sysmbols for the user's situation if necessary"
+                                        + " Your answer don't always have to be positive."},
+            {"role": "user", "content": prompt}
+        ]
+    )
+    return response.choices[0].message.content
+
+def get_tarot_reading_finetune(user_question,drawn_cards):
+
+    prompt = tarot_deck.generate_prompt(user_question, drawn_cards)
+
+    response = client.chat.completions.create(
+        #####model="gpt-4",
+        model='ft:gpt-3.5-turbo-0125:personal::9J7eW2F9',
         #response_format={ "type": "json_object" },
         messages=[
             {"role": "system", "content": "You are a tarot reader. Please interpret the cards drawn for the user."
